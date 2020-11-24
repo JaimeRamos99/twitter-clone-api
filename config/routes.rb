404fbacf9e46cc6abb5c8187990782588dcf9051
Jaime Ramos <jrossetes@uninorte.edu.co>
2020-11-24 11:25:32 -0500

    fix: receiving query params instead of body in get endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -8,27 +8,27 @@ Rails.application.routes.draw do
     post '/user', to: 'user#create'
 
     #confirmation register
-    get '/user_confirm_register', to: 'user#confirmation'
+    get '/user_confirm_register/:user/:emailcode', to: 'user#confirmation'
 
     #user login
     post '/user_login', to: 'user#login'
 
     #get followers
-    get '/followers', to: 'user#followers'
+    get '/followers/:username', to: 'user#followers'
 
     #get follows
-    get '/follows', to: 'user#follows'
+    get '/follows/:username', to: 'user#follows'
 
     #delete a user
     #delete '/user', to: 'user#destroy'
 
     #get profile info of any user, passing the username desired
-    get '/profile', to:'user#profile'
+    get '/profile/:username', to:'user#profile'
 
     #follow a user
-    post '/follow', to: 'relationfollows#create'
+    post '/follow/:followed_username', to: 'relationfollows#create'
     #unfollow user
-    delete '/follow', to: 'relationfollows#destroy'
+    delete '/follow/:followed_username', to: 'relationfollows#destroy'
 
 
     #create a tweet
