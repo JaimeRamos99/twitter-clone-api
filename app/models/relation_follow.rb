3e527f9d472c79fb8bc541ea5832339886c26474
Jaime Ramos <jrossetes@uninorte.edu.co>
2020-11-24 17:36:48 -0500

    dev: improve eficiency in dev env2

--- app/models/relation_follow.rb
+++ app/models/relation_follow.rb
@@ -2,7 +2,9 @@ class RelationFollow < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
   validates_uniqueness_of :follower_id, scope: :followed_id
-  after_create :send_email_new_follow
+  if Rails.env.production?
+    after_create :send_email_new_follow
+  end
 
 
   def send_email_new_follow
