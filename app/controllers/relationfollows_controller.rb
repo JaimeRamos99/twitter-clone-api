2a0a0c1a6431bb844e9f6d7e351dfdd5c9ef4ad9
Jaime Ramos <jrossetes@uninorte.edu.co>
2020-11-24 00:52:16 -0500

    fix: following a user just once without fire an error

--- app/controllers/relationfollows_controller.rb
+++ app/controllers/relationfollows_controller.rb
@@ -6,10 +6,16 @@ class RelationfollowsController < ApplicationController
     followed_user = User.find_by(username: params[:followed_username])
     if followed_user.present?
       if Current.user.id != followed_user.id && Current.user.authenticated
-        @rel = RelationFollow.create!(follower_id: Current.user.id,
-                                      followed_id: followed_user.id
-                                     )
-        render json: {followed: true}, status: :ok
+        followed_before = Current.user.following.find(followed_user.id)
+        if followed_before.present?
+          render json: {followed: false}, status: :not_acceptable
+        else
+          @rel = RelationFollow.create!(follower_id: Current.user.id,
+                                        followed_id: followed_user.id
+                                       )
+          render json: {followed: true}, status: :ok
+        end
+
       else
         render json: {followed: false}, status: :unauthorized
       end
