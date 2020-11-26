f5fe1876eb13535b1e3c50204e3cda27de8ea092
Jaime Ramos <jrossetes@uninorte.edu.co>
2020-11-26 16:04:32 -0500

    fix: add uniqueness validation for user_id and tweet id in retweet model

--- app/models/retweet.rb
+++ app/models/retweet.rb
@@ -2,4 +2,5 @@ class Retweet < ApplicationRecord
   belongs_to :user
   belongs_to :tweet
   validates :quote, presence: true, length: {in: 0..280}
+  validates_uniqueness_of :user_id, scope: :tweet_id
 end
