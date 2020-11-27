7cb107c484fe5e0c235bf23c986882777e476453
Jaime Ramos <jrossetes@uninorte.edu.co>
2020-11-26 22:01:02 -0500

    fix:response object in retweet count

--- app/controllers/retweet_controller.rb
+++ app/controllers/retweet_controller.rb
@@ -44,7 +44,7 @@ class RetweetController < ApplicationController
             FROM retweets
             WHERE tweet_id =#{@tweet_id}"
           matches = ActiveRecord::Base.connection.execute(sql)
-          render json: {retweets: matches}, status: :ok
+          render json: matches, status: :ok
         else
           render json: { error: "not tweet found"}, status: :ok
         end
